eb582862dcc3d10a7ff095f1a9e74dfcc81662a1
MichaelLeonffu <Leonffu.Michael@gmail.com>
2017-03-26 20:16:54 -0700

    Started the Installation script

---
+++ INSTALLATION.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#By Michael Leonffu March 26, 2017 for The Orange Alliance
+#This script moves and creates directorys for the first time installation of The Orange Alliance, should be ran in the same directory as...
+
+echo "not done yet"
